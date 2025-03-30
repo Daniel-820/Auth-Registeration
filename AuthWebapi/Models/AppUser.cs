f03966427ff54216e5ce635f5676585f54444aef
Daniel-820 <shyamsam380@gmail.com>
2025-03-31 02:59:28 +0530

    Add project files.

---
+++ AuthWebapi/Models/AppUser.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
+
+namespace AuthWebapi.Models
+{
+    public class AppUser:IdentityUser
+    {
+        [PersonalData]
+        [Column(TypeName ="nvarchar(150)")]
+        public string FullName { get; set; }
+
+    }
+}

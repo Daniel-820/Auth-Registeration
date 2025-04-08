8ad34f98c64f5d98b67fd13bc744b4fe4c402dc1
Daniel-820 <shyamsam380@gmail.com>
2025-04-08 12:10:50 +0530

    my 2nd commit after completed AuthProject

---
+++ AuthWebapi/ExtensionClasses/EFCoreExtensionConfig.cs
@@ -0,0 +1,18 @@
+﻿using AuthWebapi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthWebapi.ExtensionClasses
+{
+    public static class EFCoreExtensionConfig
+    {
+        public static IServiceCollection InjectDbContex(
+            this IServiceCollection services,
+            IConfiguration config)
+        {
+            services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlServer(config.GetConnectionString("DevDB")));
+
+            return services;
+        }
+    }
+}
--- AuthWebapi/Models/AppUser.cs
+++ AuthWebapi/Models/AppUser.cs
@@ -6,8 +6,18 @@ namespace AuthWebapi.Models
     public class AppUser:IdentityUser
     {
         [PersonalData]
-        [Column(TypeName ="nvarchar(150)")]
+        [Column(TypeName = "nvarchar(150)")]
         public string FullName { get; set; }
+
+        [PersonalData]
+        [Column(TypeName = "nvarchar(10)")]
+        public string Gender { get; set; }
+
+        [PersonalData]
+        public DateOnly DOB { get; set; }
+
+        [PersonalData]
+        public int? LibraryID { get; set; }
 
     }
 }
